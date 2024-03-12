7e18da86817f26e5ca805d5b3d7efb3046c808a5
Nelson Mbau <mbau.ndungu@gmail.com>
2024-03-12 15:18:56 +0300

    feat: added functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, desc){
+  // return names.map(name => {
+  //   return `Thank you, ${name}, for the wonderful ${desc} gift!`
+  // })
+
+  const messages = []
+
+  for(let i = 0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${desc} gift!`)
+  }
+
+  return messages
+}
+
+function countDown(num) {
+  for(let i = num; i >= 0; i--){
+    console.log(i)
+  }
+}
